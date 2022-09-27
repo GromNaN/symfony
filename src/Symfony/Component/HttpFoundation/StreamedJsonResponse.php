357330b09417647a1019038d8825ea4b11cc1784
Alexander Schranz <alexander@sulu.io>
2022-09-28 01:47:48 +0200

    Use a more common example of an object instead of string

--- src/Symfony/Component/HttpFoundation/StreamedJsonResponse.php
+++ src/Symfony/Component/HttpFoundation/StreamedJsonResponse.php
@@ -45,9 +45,9 @@ use const JSON_THROW_ON_ERROR;
  *     // array of generators with replace identifier used as key
  *     [
  *         '__articles__' => function (): \Generator { // any method or function returning a Generator
- *              yield 'Article 1';
- *              yield 'Article 2';
- *              yield 'Article 3';
+ *              yield ['title' => 'Article 1'];
+ *              yield ['title' => 'Article 2'];
+ *              yield ['title' => 'Article 3'];
  *         },
  *     ]
  * );
