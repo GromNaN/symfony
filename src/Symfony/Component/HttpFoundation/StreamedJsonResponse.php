799f0f9a3b8afe372fef8d39f973bb7b07499d7e
Alexander Schranz <alexander@sulu.io>
2022-09-27 22:03:57 +0200

    Add StreamedJsonResponse for efficient JSON streaming

---
+++ src/Symfony/Component/HttpFoundation/StreamedJsonResponse.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\HttpFoundation;
+
+use function flush;
+
+/**
+ * StreamedJsonResponse represents a streamed HTTP response for JSON.
+ *
+ * A StreamedJsonResponse uses a structure and generics to create an
+ * efficient resource saving JSON response.
+ *
+ * It uses flush after a specified flush size to directly stream the data.
+ *
+ * @see flush()
+ *
+ * @author Alexander Schranz <alexander@sulu.io>
+ */
+class StreamedJsonResponse extends StreamedResponse
+{
+    public const DEFAULT_ENCODING_OPTIONS = JsonResponse::DEFAULT_ENCODING_OPTIONS;
+
+    protected int $encodingOptions = self::DEFAULT_ENCODING_OPTIONS;
+
+    protected int $flushSize = 500;
+
+    /**
+     * @param mixed[] $structure
+     * @param array<string, \Generator<int|string, mixed> $generics
+     */
+    public function __construct(
+        private readonly array $structure,
+        private readonly array $generics,
+        int $status = 200,
+        array $headers = [],
+    ) {
+        parent::__construct(function() {
+            $keys = \array_keys($this->generics);
+
+            $jsonEncodingOptions = \JSON_THROW_ON_ERROR | $this->getEncodingOptions();
+            $structureText = \json_encode($this->structure, $jsonEncodingOptions);
+
+            foreach ($keys as $key) {
+                [$start, $end] = \explode('"' . $key . '"', $structureText, 2);
+
+                // send first and between parts of the structure
+                echo $start;
+
+                $count = 0;
+                echo '[';
+                foreach ($this->generics[$key] as $array) {
+                    if ($count !== 0) {
+                        // if not first element of the generic a separator is required between the elements
+                        echo ',';
+                    }
+
+                    echo \json_encode($array, $jsonEncodingOptions);
+                    ++$count;
+                }
+                echo ']';
+
+                if ($count % $this->flushSize === 0) {
+                    flush();
+                }
+
+                $structureText = $end;
+            }
+
+            echo $structureText; // send the after part of the structure as last
+        }, $status, $headers);
+
+        $this->headers->set('Content-Type', 'application/json');
+    }
+
+    /**
+     * Returns options used while encoding data to JSON.
+     */
+    public function getEncodingOptions(): int
+    {
+        return $this->encodingOptions;
+    }
+
+    /**
+     * Sets options used while encoding data to JSON.
+     *
+     * @return $this
+     */
+    public function setEncodingOptions(int $encodingOptions): static
+    {
+        $this->encodingOptions = $encodingOptions;
+
+        return $this;
+    }
+
+    /**
+     * Sets the flush size.
+     *
+     * @return int
+     */
+    public function getFlushSize(): int
+    {
+        return $this->flushSize;
+    }
+
+    /**
+     * Returns the flush size.
+     *
+     * @param int $flushSize
+     */
+    public function setFlushSize(int $flushSize): void
+    {
+        $this->flushSize = $flushSize;
+    }
+}
---
+++ src/Symfony/Component/HttpFoundation/Tests/StreamedJsonResponseTest.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\HttpFoundation\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\StreamedJsonResponse;
+
+class StreamedJsonResponseTest extends TestCase
+{
+    public function testResponseSimpleList(): void
+    {
+        $content = $this->createSendResponse(
+            [
+                '_embedded' => [
+                    'articles' => '__articles__',
+                    'news' => '__news__',
+                ],
+            ],
+            [
+                '__articles__' => $this->generatorSimple('Article'),
+                '__news__' => $this->generatorSimple('News'),
+            ],
+        );
+
+        $this->assertSame('{"_embedded":{"articles":["Article 1","Article 2","Article 3"],"news":["News 1","News 2","News 3"]}}', $content);
+    }
+
+    public function testResponseObjectsList(): void
+    {
+        $content = $this->createSendResponse(
+            [
+                '_embedded' => [
+                    'articles' => '__articles__',
+                ],
+            ],
+            [
+                '__articles__' => $this->generatorArray('Article'),
+            ],
+        );
+
+        $this->assertSame('{"_embedded":{"articles":[{"title":"Article 1"},{"title":"Article 2"},{"title":"Article 3"}]}}', $content);
+    }
+
+    public function testResponseStatusCode(): void
+    {
+        $response = new StreamedJsonResponse([], [], 201);
+
+        $this->assertSame(201, $response->getStatusCode());
+    }
+
+    public function testResponseHeaders(): void
+    {
+        $response = new StreamedJsonResponse([], [], 200, ['X-Test' => 'Test']);
+
+        $this->assertSame('Test', $response->headers->get('X-Test'));
+    }
+
+    public function testFlushSize(): void
+    {
+        $response = new StreamedJsonResponse([], []);
+        $response->setFlushSize(50);
+
+        $this->assertSame(50, $response->getFlushSize());
+    }
+
+    public function testEncodingOptions(): void
+    {
+        $response = new StreamedJsonResponse([], []);
+        $response->setEncodingOptions(\JSON_UNESCAPED_SLASHES);
+
+        $this->assertSame(\JSON_UNESCAPED_SLASHES, $response->getEncodingOptions() & \JSON_UNESCAPED_SLASHES);
+    }
+
+    /**
+     * @param mixed[] $structure
+     * @param array<string, \Generator<int|string, mixed> $generics
+     */
+    private function createSendResponse(
+        array $structure,
+        array $generics,
+    ): string {
+        $response = new StreamedJsonResponse($structure, $generics);
+
+        ob_start();
+        $response->send();
+
+        return ob_get_clean();
+    }
+
+    /**
+     * @return \Generator<int, string>
+     */
+    private function generatorSimple(string $test): \Generator
+    {
+        yield $test . ' 1';
+        yield $test . ' 2';
+        yield $test . ' 3';
+    }
+
+    /**
+     * @return \Generator<int, string>
+     */
+    private function generatorArray(string $test): \Generator
+    {
+        yield ['title' => $test . ' 1'];
+        yield ['title' => $test . ' 2'];
+        yield ['title' => $test . ' 3'];
+    }
+}
